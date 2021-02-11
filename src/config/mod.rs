e395c18e3e36672bd11f08dd27a93cda21f6de67
Johannes Maria Frank <jmfrank63@gmail.com>
2021-02-11 23:34:44 +0000

    during first test implementation (broken)

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,6 +1,6 @@
-use serde::Deserialize;
-use dotenv::dotenv;
 use config::{Config, ConfigError, Environment};
+use dotenv::dotenv;
+use serde::Deserialize;
 
 #[derive(Deserialize)]
 pub struct ServerConfig {
@@ -9,18 +9,15 @@ pub struct ServerConfig {
 }
 
 #[derive(Deserialize)]
-pub  struct S3Config {
+pub struct RS3Config {
     pub server: ServerConfig,
 }
 
-
-impl S3Config {
+impl RS3Config {
     pub fn from_env() -> Result<Self, ConfigError> {
-
         dotenv().ok();
         let mut s3_config = Config::new();
         s3_config.merge(Environment::new()).unwrap();
         s3_config.try_into()
     }
-
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,50 @@
-fn main() {
-    println!("Hello, world!");
+mod config;
+
+use crate::config::RS3Config;
+
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use std::io;
+
+#[actix_rt::main]
+pub async fn main() -> io::Result<()> {
+    let rs3_conf = RS3Config::from_env().unwrap();
+
+    println!(
+        "Starting Http server at host address: {}, with port: {}!",
+        rs3_conf.server.host, rs3_conf.server.port
+    );
+
+    HttpServer::new(move || App::new().configure(app_config))
+        .bind(format!("{}:{}", rs3_conf.server.host, rs3_conf.server.port))?
+        .run()
+        .await
+}
+
+fn app_config(config: &mut web::ServiceConfig) {
+    config.route("/", web::get().to(index));
+}
+
+async fn index(_req: HttpRequest) -> impl Responder {
+    HttpResponse::Ok().json("{'Status' : 'Up'}")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use actix_web::{http, test};
+    use std::io::Read;
+
+    #[actix_rt::test]
+    async fn test_index_ok() {
+        let req = test::TestRequest::with_header("content-type", "text/plain").to_http_request();
+        let resp = index(req).await;
+        assert!(resp.status().is_ok());
+    }
+
+    #[actix_rt::test]
+    async fn test_index_not_ok() {
+        let req = test::TestRequest::default().to_http_request();
+        let resp = index(req).await;
+        assert!(resp.status().is_err());
+    }
 }
