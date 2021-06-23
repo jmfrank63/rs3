d997443fcab455d5e9d92c2e044029f32e2b5606
Johannes Maria Frank <jmfrank63@gmail.com>
2021-06-23 17:11:33 +0100

    Basic adding and removal with default hasher

--- rendezvous_hashing/src/main.rs
+++ rendezvous_hashing/src/main.rs
@@ -1,34 +1,40 @@
+use rendezvous_hash::{Capacity, DefaultNodeHasher, WeightedNode};
+use rendezvous_hash::{Node, RendezvousNodes};
 use std::collections::HashMap;
-use rendezvous_hash::{RendezvousNodes, Node};
-use rendezvous_hash::{WeightedNode, Capacity};
 
 fn main() {
-    let mut nodes = RendezvousNodes::default();
-    nodes.insert(WeightedNode::new("foo", Capacity::new(70.0).unwrap()));
-    nodes.insert(WeightedNode::new("bar", Capacity::new(20.0).unwrap()));
-    nodes.insert(WeightedNode::new("baz", Capacity::new(9.0).unwrap()));
-    nodes.insert(WeightedNode::new("qux", Capacity::new(1.0).unwrap()));
-    println!("{}", nodes.len());
+    let mut weighted_nodes = RendezvousNodes::default();
+    // nodes.insert(WeightedNode::new("foo", Capacity::new(70.0).unwrap()));
+    weighted_nodes.insert(WeightedNode::new("bar", Capacity::new(20.0).unwrap()));
+    weighted_nodes.insert(WeightedNode::new("baz", Capacity::new(9.0).unwrap()));
+    weighted_nodes.insert(WeightedNode::new("qux", Capacity::new(1.0).unwrap()));
+    println!("{}", weighted_nodes.len());
 
-    for node in nodes.iter() {
+    for node in weighted_nodes.iter() {
         println!("{}", node.node_id());
     }
 
-    // nodes.remove(&"foo");
-    // nodes.remove(&"qux");
-    println!("{}", nodes.len());
+    weighted_nodes.remove(&"foo");
+    weighted_nodes.remove(&"qux");
+    println!("{}", weighted_nodes.len());
 
     let mut counts = HashMap::new();
-    let amount = 121;
-    for item in 0..amount {
-        let node = nodes.calc_candidates(&item).nth(0).unwrap();
-        if node.node_id() == &"qux" {
-            println!("{}, {}", node.node_id(), node.node.to_string());
-        }
+    let amount = 10000;
+    add_map_entries(&mut weighted_nodes, &mut counts, amount);
+    for key in counts.iter() {
+        println!("{}", counts[key.0]);
+    }
+}
 
+fn add_map_entries(
+    weighted_nodes: &mut RendezvousNodes<WeightedNode<&str>, DefaultNodeHasher>,
+    counts: &mut HashMap<String, i32>,
+    amount: i32,
+) {
+    for item in 0..amount {
+        let node = weighted_nodes.calc_candidates(&item).nth(0).unwrap();
         *counts.entry(node.node.to_string()).or_insert(0) += 1;
     }
-
 }
 
 #[cfg(test)]
@@ -43,15 +49,24 @@ mod tests {
         nodes.insert(WeightedNode::new("baz", Capacity::new(9.0).unwrap()));
         nodes.insert(WeightedNode::new("qux", Capacity::new(1.0).unwrap()));
 
-        let mut counts = HashMap::new();
-        let amount = 10000;
-        for item in 0..amount {
-            let node = nodes.calc_candidates(&item).nth(0).unwrap();
-            *counts.entry(node.node.to_string()).or_insert(0) += 1;
-        }
-        assert_eq!(((counts["foo"] as f64) / (amount as f64 / 100.0)).round(), 70.0);
-        assert_eq!(((counts["bar"] as f64) / (amount as f64 / 100.0)).round(), 20.0);
-        assert_eq!(((counts["baz"] as f64) / (amount as f64 / 100.0)).round(), 9.0);
-        assert_eq!(((counts["qux"] as f64) / (amount as f64 / 100.0)).round(), 1.0);
+        let mut counts: HashMap<String, u32> = HashMap::new();
+        let amount: u32 = 10000;
+        add_map_entries(&mut weighted_nodes, &mut counts, amount);
+        assert_eq!(
+            ((counts["foo"] as f64) / (amount as f64 / 100.0)).round(),
+            70.0
+        );
+        assert_eq!(
+            ((counts["bar"] as f64) / (amount as f64 / 100.0)).round(),
+            20.0
+        );
+        assert_eq!(
+            ((counts["baz"] as f64) / (amount as f64 / 100.0)).round(),
+            9.0
+        );
+        assert_eq!(
+            ((counts["qux"] as f64) / (amount as f64 / 100.0)).round(),
+            1.0
+        );
     }
 }
