d83ecc021257f88591435746427af4dccd439a68
Johannes Maria Frank <jmfrank63@gmail.com>
2021-08-31 16:21:53 +0200

    Improved ansible docker service restarting and better service test

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod config;
 
 use crate::config::Config;
 
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer};
+use actix_web::{get, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 use std::io;
 
 #[actix_web::main]
@@ -20,11 +20,13 @@ pub async fn main() -> io::Result<()> {
         .await
 }
 
+
 fn app_config(config: &mut web::ServiceConfig) {
-    config.route("/", web::get().to(index));
+    config.service(index);
 }
 
-async fn index(_req: HttpRequest) -> HttpResponse {
+#[get("/")]
+async fn index(_req: HttpRequest) -> impl Responder {
     let rs3_conf = Config::from_env().unwrap();
     HttpResponse::Ok().json("{'IP' : '".to_string() + rs3_conf.server.host.as_str() + "'}")
 }
@@ -33,14 +35,14 @@ async fn index(_req: HttpRequest) -> HttpResponse {
 mod tests {
     use super::*;
     use actix_web::http::StatusCode;
-    use actix_web::test;
+    use actix_web::{test, App};
 
     #[actix_rt::test]
     async fn test_index_ok() {
-        let req = test::TestRequest::default()
-            .insert_header(("content-type", "text/plain"))
-            .to_http_request();
-        let resp = index(req).await;
+        let mut app = test::init_service(App::new().service(index)).await;
+        let req = test::TestRequest::default().insert_header(("content-type", "text/plain"))
+            .to_request();
+        let resp = test::call_service(&mut app, req).await;
         assert_eq!(resp.status(), StatusCode::OK);
     }
 }
