ef2951c114d670512db1a0a37d1df08696eb1a2e
Johannes Maria Frank <jmfrank63@gmail.com>
2021-09-10 20:54:42 +0200

    Added more http requests

--- docker_rs3/ansible_start_rs3.sh
+++ docker_rs3/ansible_start_rs3.sh
@@ -1,2 +1,2 @@
 #/bin/bash
-ansible-playbook -u ubuntu --private-key ~/.ssh/rs3_london.pem -i rs3.common-work-education.co.uk, playbook_prod.yml
+ansible-playbook -u ubuntu --private-key ~/.ssh/rs3_london.pem -i rs3.common-work-education.co.uk, playbook_rs3.yml
